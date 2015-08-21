2776b811c269759e303c79edc895c3d26a108d73
smartyaunt <401434286@qq.com>
2015-08-22 01:25:45 +0800

    微信验证（3）

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
 
 define('TOKEN', '199008yyb');
-function checkSignature() {
-    $signature = $_GET["signature"];
-    $timestamp = $_GET["timestamp"];
-    $nonce = $_GET["nonce"];	
-        		
-	$token = TOKEN;
-	$tmpArr = array($token, $timestamp, $nonce);
-	sort($tmpArr, SORT_STRING);
-	$tmpStr = implode( $tmpArr );
-	$tmpStr = sha1( $tmpStr );
-	
-	if( $tmpStr == $signature ){
-		return true;
-	}else{
-		return false;
-	}
+
+$signature = $_GET["signature"];
+$timestamp = $_GET["timestamp"];
+$nonce = $_GET["nonce"];	
+    		
+$token = TOKEN;
+$tmpArr = array($token, $timestamp, $nonce);
+sort($tmpArr, SORT_STRING);
+$tmpStr = implode( $tmpArr );
+$tmpStr = sha1( $tmpStr );
+
+if( $tmpStr == $signature ){
+	echo $_GET['echostr'];
+}else{
+	return false;
 }
-checkSignature();
+
 ?>
