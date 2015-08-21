d693375b494c10996af68056d4adf4677281dd62
smartyaunt <401434286@qq.com>
2015-08-22 01:34:04 +0800

    微信验证（5）

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $signature = $_GET["signature"];
 $timestamp = $_GET["timestamp"];
 $nonce = $_GET["nonce"];	
     		
-$token = '199008yyb';
+$token = '199008yybcb';
 $tmpArr = array($token, $timestamp, $nonce);
 sort($tmpArr, SORT_STRING);
 $tmpStr = implode( $tmpArr );
